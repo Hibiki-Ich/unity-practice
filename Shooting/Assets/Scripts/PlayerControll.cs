b6d6243172d3506704c03ff52a717dc9a826430e
Hibiki-Ich <hibi628@g.ecc.u-tokyo.ac.jp>
2023-10-22 15:18:16 +0900

    add shooting to hibi

---
+++ Shooting/Assets/Scripts/PlayerControll.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.Windows;
+
+public class PlayerControll : MonoBehaviour{
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(Input.GetKey(KeyCode.A)){
+            transform.Translate(-0.06f, 0, 0);
+        }
+        else if(Input.GetKey(KeyCode.D)){
+            transform.Translate(0.06f, 0, 0);
+        }
+        else if(Input.GetKey(KeyCode.W)){
+            transform.Translate(0, 0, 0.06f);
+        }
+        else if(Input.GetKey(KeyCode.S)){
+            transform.Translate(0, 0, -0.06f);
+        }
+    }
+}
